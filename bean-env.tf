ad6d76d25985722ae3fd5bb8025d059ce6b9a2ce
lokesh <lokeshambati@123>
2023-08-04 11:48:32 +0530

    Bean Env Updated

--- bean-env.tf
+++ bean-env.tf
@@ -1,4 +1,4 @@
-resource "aws_elastic_beanstalk_application" "vprofile-prod" {
+resource "aws_elastic_beanstalk_environment" "vprofile-prod" {
   name = "vprofile-prod"
   application = aws_elastic_beanstalk_application.vprofile-prod
   solution_stack_name = "64bit Amazon Linux 2 v4.3.10 running Tomcat 8.5 Corretto 11"
